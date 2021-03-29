4adcd99e5bde1b072e5496ed6caf906767c6296b
MrDeatHHH <37218089+MrDeatHHH@users.noreply.github.com>
2021-03-29 19:01:54 +0200

    Max -> min, fixed sign in formula, added 3 colors

--- Geom_lab_4.cpp
+++ Geom_lab_4.cpp
@@ -12,45 +12,52 @@ using namespace cv;
 using namespace std;
 using namespace std::chrono;
 
-int qFunc(int i, int j, int k, const int N, int*** colors)
+int qFunc(int i, int j, int k, const int N, int**** colors)
 {
-	if (colors[k][i][j] != 0)
-		return 0;
-	else
-	{
-		bool all = true;
-		for (int k_ = 0; k_ < N; ++k_)
-			if (colors[k_][i][j] != 0)
+	for (int c = 0; c < 3; ++c)
+		if (colors[k][c][i][j] != 0)
+			return 0;
+
+	bool all = true;
+	for (int k_ = 0; k_ < N; ++k_)
+		for (int c = 0; c < 3; ++c)
+			if (colors[k_][c][i][j] != 0)
 				all = false;
 
-		if (all)
-			return 0;
-		else
-			return -100000000;
-	}
+	if (all)
+		return 0;
+	else
+		return 1000000;
 }
 
-int g(int t, int t_, int k, int k_, const int width, int*** colors)
+float norm(int k, int k_, int i, int j, int**** colors)
 {
-	return abs(colors[k][t_ / width][t_ % width] - colors[k_][t_ / width][t_ % width]) - abs(colors[k][t / width][t % width] - colors[k_][t / width][t % width]);
+	return pow(pow(colors[k][0][i][j] - colors[k_][0][i][j], 2) +
+		       pow(colors[k][1][i][j] - colors[k_][1][i][j], 2) +
+		       pow(colors[k][2][i][j] - colors[k_][2][i][j], 2), 0.5);
 }
 
-int** run(const int height, const int width, const int modKs, int*** q, int*** colors, const int loops)
+float g(int t, int t_, int k, int k_, const int width, int**** colors)
+{
+	return norm(k, k_, t / width, t % width, colors) + norm(k, k_, t_ / width, t_ % width, colors);
+}
+
+int** run(const int height, const int width, const int modKs, int*** q, int**** colors, const int loops)
 {
 	// Initialize phi, L, R, U, D
 	int modT = height * width;
-	double** phi = new double* [modT];
-	double** L = new double* [modT];
-	double** R = new double* [modT];
-	double** U = new double* [modT];
-	double** D = new double* [modT];
+	float** phi = new float* [modT];
+	float** L = new float* [modT];
+	float** R = new float* [modT];
+	float** U = new float* [modT];
+	float** D = new float* [modT];
 	for (int ij = 0; ij < modT; ++ij)
 	{
-		phi[ij] = new double[modKs]();
-		L[ij] = new double[modKs]();
-		R[ij] = new double[modKs]();
-		U[ij] = new double[modKs]();
-		D[ij] = new double[modKs]();
+		phi[ij] = new float[modKs]();
+		L[ij] = new float[modKs]();
+		R[ij] = new float[modKs]();
+		U[ij] = new float[modKs]();
+		D[ij] = new float[modKs]();
 	}
 
 	for (int i = height - 2; i >= 0; --i)
@@ -61,23 +68,23 @@ int** run(const int height, const int width, const int modKs, int*** q, int*** c
 			const int ij = i_ + j;
 			for (int k = 0; k < modKs; ++k)
 			{
-				double maxR = -10000000.;
+				float minR = 100000.;
 				for (int k_ = 0; k_ < modKs; ++k_)
 				{
-					const double R_ = R[ij + 1][k_] + 0.5 * q[i][j + 1][k_] + g(ij + 1, ij, k_, k, width, colors);
-					if (R_ > maxR)
-						maxR = R_;
+					const float R_ = R[ij + 1][k_] + 0.5 * q[i][j + 1][k_] + g(ij + 1, ij, k_, k, width, colors);
+					if (R_ < minR)
+						minR = R_;
 				}
-				R[i_ + j][k] = maxR;
+				R[i_ + j][k] = minR;
 
-				double maxD = -10000000.;
+				float minD = 100000.;
 				for (int k_ = 0; k_ < modKs; ++k_)
 				{
-					const double D_ = D[ij + width][k_] + 0.5 * q[i + 1][j][k_] + g(ij + width, ij, k_, k, width, colors);
-					if (D_ > maxD)
-						maxD = D_;
+					const float D_ = D[ij + width][k_] + 0.5 * q[i + 1][j][k_] + g(ij + width, ij, k_, k, width, colors);
+					if (D_ < minD)
+						minD = D_;
 				}
-				D[i_ + j][k] = maxD;
+				D[i_ + j][k] = minD;
 			}
 		}
 	}
@@ -95,23 +102,23 @@ int** run(const int height, const int width, const int modKs, int*** q, int*** c
 				const int ij = i_ + j;
 				for (int k = 0; k < modKs; ++k)
 				{
-					double maxL = -10000000.;
+					float minL = 100000.;
 					for (int k_ = 0; k_ < modKs; ++k_)
 					{
-						const double L_ = L[ij - 1][k_] + 0.5 * q[i][j - 1][k_] + g(ij - 1, ij, k_, k, width, colors) - phi[ij - 1][k_];
-						if (L_ > maxL)
-							maxL = L_;
+						const float L_ = L[ij - 1][k_] + 0.5 * q[i][j - 1][k_] + g(ij - 1, ij, k_, k, width, colors) - phi[ij - 1][k_];
+						if (L_ < minL)
+							minL = L_;
 					}
-					L[ij][k] = maxL;
+					L[ij][k] = minL;
 
-					double maxU = -10000000.;
+					float minU = 100000.;
 					for (int k_ = 0; k_ < modKs; ++k_)
 					{
-						const double U_ = U[ij - width][k_] + 0.5 * q[i - 1][j][k_] + g(ij - width, ij, k_, k, width, colors) + phi[ij - width][k_];
-						if (U_ > maxU)
-							maxU = U_;
+						const float U_ = U[ij - width][k_] + 0.5 * q[i - 1][j][k_] + g(ij - width, ij, k_, k, width, colors) + phi[ij - width][k_];
+						if (U_ < minU)
+							minU = U_;
 					}
-					U[ij][k] = maxU;
+					U[ij][k] = minU;
 
 					phi[ij][k] = (L[ij][k] + R[ij][k] - U[ij][k] - D[ij][k]) * 0.5;
 				}
@@ -126,23 +133,23 @@ int** run(const int height, const int width, const int modKs, int*** q, int*** c
 				const int ij = i_ + j;
 				for (int k = 0; k < modKs; ++k)
 				{
-					double maxR = -10000000.;
+					float minR = 100000.;
 					for (int k_ = 0; k_ < modKs; ++k_)
 					{
-						const double R_ = R[ij + 1][k_] + 0.5 * q[i][j + 1][k_] + g(ij + 1, ij, k_, k, width, colors) - phi[ij + 1][k_];
-						if (R_ > maxR)
-							maxR = R_;
+						const float R_ = R[ij + 1][k_] + 0.5 * q[i][j + 1][k_] + g(ij + 1, ij, k_, k, width, colors) - phi[ij + 1][k_];
+						if (R_ < minR)
+							minR = R_;
 					}
-					R[ij][k] = maxR;
+					R[ij][k] = minR;
 
-					double maxD = -10000000.;
+					float minD = 100000.;
 					for (int k_ = 0; k_ < modKs; ++k_)
 					{
-						const double D_ = D[ij + width][k_] + 0.5 * q[i + 1][j][k_] + g(ij + width, ij, k_, k, width, colors) + phi[ij + width][k_];
-						if (D_ > maxD)
-							maxD = D_;
+						const float D_ = D[ij + width][k_] + 0.5 * q[i + 1][j][k_] + g(ij + width, ij, k_, k, width, colors) + phi[ij + width][k_];
+						if (D_ < minD)
+							minD = D_;
 					}
-					D[ij][k] = maxD;
+					D[ij][k] = minD;
 
 					phi[ij][k] = (L[ij][k] + R[ij][k] - U[ij][k] - D[ij][k]) * 0.5;
 				}
@@ -151,7 +158,7 @@ int** run(const int height, const int width, const int modKs, int*** q, int*** c
 	}
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
-	cout << "Time used for " << loops << " iterations : " << double(duration.count()) / 1000000. << endl;
+	cout << "Time used for " << loops << " iterations : " << float(duration.count()) / 1000000. << endl;
 
 	// Best Ks
 	int** res = new int* [height];
@@ -167,11 +174,11 @@ int** run(const int height, const int width, const int modKs, int*** q, int*** c
 		{
 			const int ij = i_ + j;
 			int k_star = 0;
-			double value = -10000000.;
+			float value = 100000.;
 			for (int k_ = 0; k_ < modKs; ++k_)
 			{
-				const double v_ = L[ij][k_] + R[ij][k_] + 0.5 * q[i][j][k_] - phi[ij][k_];
-				if (v_ > value)
+				const float v_ = L[ij][k_] + R[ij][k_] + 0.5 * q[i][j][k_] - phi[ij][k_];
+				if (v_ < value)
 				{
 					value = v_;
 					k_star = k_;
@@ -206,21 +213,26 @@ int main()
 	int width = 0;
 
 	// Get array from Mat
-	int*** colors = new int** [N];
+	int**** colors = new int*** [N];
 	for (int k = 0; k < N; ++k)
 	{
-		Mat image_, image;
-		image_ = imread("res" + to_string(k + 1) + ".png", IMREAD_UNCHANGED);
-		cvtColor(image_, image, COLOR_BGR2GRAY);
-		height = image.size().height;
-		width = image.size().width;
-		colors[k] = new int* [height];
-		for (int i = 0; i < height; ++i)
+		vector<Mat> channels(3);
+		Mat image;
+		image = imread("res" + to_string(k + 1) + ".png", IMREAD_UNCHANGED);
+		split(image, channels);
+		height = channels[0].size().height;
+		width = channels[0].size().width;
+		colors[k] = new int** [3];
+		for (int c = 0; c < 3; ++c)
 		{
-			colors[k][i] = new int[width];
-			for (int j = 0; j < width; ++j)
+			colors[k][c] = new int* [height];
+			for (int i = 0; i < height; ++i)
 			{
-				colors[k][i][j] = int(image.at<uchar>(i, j));
+				colors[k][c][i] = new int[width];
+				for (int j = 0; j < width; ++j)
+				{
+					colors[k][c][i][j] = int(channels[c].at<uchar>(i, j));
+				}
 			}
 		}
 	}
@@ -229,7 +241,7 @@ int main()
 	const int epsilon = 0;
 	const int modKs = N;
 	const int modT = height * width;
-	
+
 	// Q
 	int*** q = new int** [height];
 	for (int i = 0; i < height; ++i)
@@ -243,13 +255,20 @@ int main()
 		}
 	}
 
-	const int loops = 30;
+	const int loops = 10;
 	int** res = run(height, width, modKs, q, colors, loops);
 
-	Mat result = Mat::zeros(Size(width, height), CV_8UC1);
-	for (int i = 0; i < height; ++i)
-		for (int j = 0; j < width; ++j)
-			result.at<uchar>(i, j) = uchar(colors[res[i][j]][i][j]);
+	//then merge them back
+	Mat result, chanels[3];
+	for (int c = 0; c < 3; ++c)
+	{
+		chanels[c] = Mat::zeros(Size(width, height), CV_8UC1);
+		for (int i = 0; i < height; ++i)
+			for (int j = 0; j < width; ++j)
+				chanels[c].at<uchar>(i, j) = uchar(colors[res[i][j]][c][i][j]);
+	}
+
+	merge(chanels, 3, result);
 
 	namedWindow("Result image", WINDOW_AUTOSIZE);
 	imshow("Result image", result);
